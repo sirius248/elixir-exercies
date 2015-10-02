72eaa380f78c1444823be95ccc84a08232f8708d
Long Nguyen <long.polyglot@gmail.com>
2015-10-02 16:19:03 +0700

    Solution for bank account

---
+++ bank-account/account.exs
@@ -0,0 +1,56 @@
+defmodule BankAccount do
+  @moduledoc """
+  A bank account that supports access from multiple processes.
+  """
+
+  @typedoc """
+  An account handle.
+  """
+  @opaque account :: pid
+
+  @doc """
+  Open the bank. Makes the account available.
+  """
+  @spec open_bank() :: account
+  def open_bank() do
+    BalanceAgent.new
+  end
+
+  @doc """
+  Close the bank. Makes the account unavailable.
+  """
+  @spec close_bank(account) :: none
+  def close_bank(_account) do
+  end
+
+  @doc """
+  Get the account's balance.
+  """
+  @spec balance(account) :: integer
+  def balance(account) do
+    BalanceAgent.get(account)
+  end
+
+  @doc """
+  Update the account's balance by adding the given amount which may be negative.
+  """
+  @spec update(account, integer) :: any
+  def update(account, amount) do
+    BalanceAgent.add(account, amount)
+  end
+end
+
+defmodule BalanceAgent do
+  def new do
+    {:ok, pid} = Agent.start_link(fn -> 0 end)
+    pid
+  end
+
+  def get(pid) do
+    Agent.get(pid, fn (balance) -> balance end)
+  end
+
+  def add(pid, amount) do
+    Agent.update(pid, fn (balance) -> balance + amount end)
+  end
+end
